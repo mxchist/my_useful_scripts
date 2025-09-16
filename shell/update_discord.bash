1075e0a42e35a8d69fd5a793af4f502a0ab60f2e
mxchist <48618148+mxchist@users.noreply.github.com>
2025-09-16 19:04:53 +0400

    Update update_discord.bash
    Fixed bug with remote_version evaluation.

--- shell/update_discord.bash
+++ shell/update_discord.bash
@@ -2,7 +2,8 @@
 # Update the Discord using cron. Tested under Ubuntu + Gnome.
 set -e
 
-declare remote_version=$(curl -o /dev/null -w "%{redirect_url}" -silent https://discord.com/api/download?platform=linux\&format=deb)
+declare redirect_url=$(curl -o /dev/null -w "%{redirect_url}" -silent https://discord.com/api/download?platform=linux\&format=deb)
+declare remote_version=$(echo -n "$redirect_url" | sed -E "s/.*\/([^\/]+)\/discord\-\1\.deb/\1/g" - )
 declare installed_version=$(dpkg-query --showformat='${Version}' -W discord)
 if [[ $installed_version != $remote_version ]]; then
     curl -L --output-dir ~/Downloads/ --output discord.deb --silent https://discord.com/api/download?platform=linux\&format=deb;
