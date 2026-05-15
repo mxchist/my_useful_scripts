50673b2d22f222c718041628e82bb5e3de0b198d
Chistyakov Maksim <gods.like.you@gmail.com>
2026-05-15 18:20:31 +0400

    The configuraion well-developed since base AI version, though not ready yet

--- system_config/nixos/etc/nixos/telegram-instances.nix
+++ system_config/nixos/etc/nixos/telegram-instances.nix
@@ -1,46 +1,85 @@
 { pkgs, ... }:
 
 let
-  mkTelegramInstance = { name, displayName, workdir, iconName }:
+  mkIconPackage = { name, iconsDir }:
+    pkgs.runCommand "${name}-icons" {} ''
+      for size in 16 24 32 48 64 128 256; do
+        if [ -f ${iconsDir}/''${size}x''${size}.png ]; then
+          install -Dm644 ${iconsDir}/''${size}x''${size}.png \
+            $out/share/icons/hicolor/''${size}x''${size}/apps/${name}.png
+        fi
+      done
+    '';
+
+  mkTelegramInstance = { name, displayName, workdir, iconsDir }:
     let
       desktopItem = pkgs.makeDesktopItem {
-        name = name;                      # имя .desktop-файла (без расширения)
-        desktopName = displayName;        # видимое имя в меню GNOME
-        exec = "${name} %U";              # команда запуска
-        icon = iconName;                  # имя иконки (резолвится через icon theme)
-        categories = [ "Network" "InstantMessaging" ];
-        startupWMClass = name;            # для группировки окон в доке
-        mimeTypes = [ "x-scheme-handler/tg" ];
+        name = name;
+        desktopName = displayName;
+	comment = "New era of messaging";
+	tryExec = name;
+        exec = "${name} -- %U";
+        icon = name;
+	terminal = false;
+        startupWMClass = name;
+        #startupWMClass = "TelegramDesktop";
+	type = "Application";
+        categories = [ "Chat" "Network" "InstantMessaging" "Qt" ];
+        mimeTypes = [ "x-scheme-handler/tg" "x-scheme-handler/tonsite"];
+	keywords = ["tg" "chat" "im" "messaging" "messenger" "sms" "tdesktop"];
+	actions.quit = {
+	  name = "Quit Telegram";
+	  exec = "Telegram -quit";
+	  icon = "application-exit";
+	};
+	dbusActivatable = true;
+	extraConfig = {
+          "DBusActivatable" = "false";
+          "SingleMainWindow" = "true";
+          "X-GNOME-UsesNotifications" = "true";
+          "X-GNOME-SingleWindow" = "true";
+      };
+
       };
     in
     pkgs.symlinkJoin {
       inherit name;
       paths = [
         (pkgs.writeShellScriptBin name ''
+        # Подсказка для Qt/Wayland: с каким .desktop ассоциировать окно
+        export QT_WAYLAND_DESKTOP_FILE="${name}.desktop"
+
+        if [ "$1" = "-quit" ]; then
           exec ${pkgs.telegram-desktop}/bin/telegram-desktop \
-            -workdir "${workdir}" "$@"
+            -workdir "${workdir}" -quit
+        fi
+        exec ${pkgs.telegram-desktop}/bin/telegram-desktop \
+          -workdir "${workdir}" \
+          -name "${name}" \
+          "$@"
         '')
         desktopItem
+        (mkIconPackage { inherit name; inherit iconsDir; })
       ];
     };
 
 in
 {
-  environment.systemPackages = with pkgs; [
-    telegram-desktop  # сам пакет нужен, чтобы был telegram-desktop в стора
+  users.users.<username>.packages = with pkgs; [               #put here your username instead of <username>
+    telegram-desktop
 
     (mkTelegramInstance {
       name = "telegram-personal";
-      displayName = "Telegram (Личный)";
-      workdir = "$HOME/.telegram-personal";
-      iconName = "telegram-personal";
+      displayName = "Telegram (Personal)";
+      workdir = "$HOME/.local/share/TelegramDesktop";
+      iconsDir = ./icons/personal;
     })
 
     (mkTelegramInstance {
       name = "telegram-work";
-      displayName = "Telegram (Работа)";
-      workdir = "$HOME/.telegram-work";
-      iconName = "telegram-work";
+      displayName = "Telegram (Work)";
+      workdir = "$HOME/.local/share/TelegramDesktopWorking";
+      iconsDir = ./icons/work;
     })
   ];
 }
