4acae915b30f40602567d3cea26e322bda2387d4
Chistyakov Maksim <gods.like.you@gmail.com>
2026-05-19 23:37:31 +0400

    Telegram instances Nixos config: improved identation

--- system_config/nixos/etc/nixos/telegram-instances.nix
+++ system_config/nixos/etc/nixos/telegram-instances.nix
@@ -2,25 +2,25 @@
 
 let
 
-  homePath = "${config.users.users.<username>.home}" ;  #put here your username instead of <username>
+  homePath = "${config.users.users.<username>.home}"; #put here your username instead of <username>
 
   mkIconPackage = { name, iconsDir }:
-    pkgs.runCommand "${name}-icons" {} ''
-      #echo "out directory is: " $out
-      #set -v;
-      for size in 16 24 32 48 64 128 256 512; do
-	if [ -f ${iconsDir}/''${size}x''${size}/apps/org.telegram.desktop.png ]; then
-          mkdir -p $out/share/icons/hicolor/''${size}x''${size}/apps;
-          install -Dm644 ${iconsDir}/''${size}x''${size}/apps/org.telegram.desktop.png \
-            $out/share/icons/hicolor/''${size}x''${size}/apps/${name}.png;
-	fi;
-      done;
+    pkgs.runCommand "${name}-icons" { } ''
+            #echo "out directory is: " $out
+            #set -v;
+            for size in 16 24 32 48 64 128 256 512; do
+      	if [ -f ${iconsDir}/''${size}x''${size}/apps/org.telegram.desktop.png ]; then
+                mkdir -p $out/share/icons/hicolor/''${size}x''${size}/apps;
+                install -Dm644 ${iconsDir}/''${size}x''${size}/apps/org.telegram.desktop.png \
+                  $out/share/icons/hicolor/''${size}x''${size}/apps/${name}.png;
+      	fi;
+            done;
 
-      mkdir -p $out/share/icons/hicolor/symbolic/apps;
-      for suffix in "-attention" "-mute" ""; do
-        install -Dm644 ${iconsDir}/symbolic/apps/org.telegram.desktop''${suffix}-symbolic.svg \
-          $out/share/icons/hicolor/symbolic/apps/${name}-''${suffix}-symbolic.svg
-      done;
+            mkdir -p $out/share/icons/hicolor/symbolic/apps;
+            for suffix in "-attention" "-mute" ""; do
+              install -Dm644 ${iconsDir}/symbolic/apps/org.telegram.desktop''${suffix}-symbolic.svg \
+                $out/share/icons/hicolor/symbolic/apps/${name}-''${suffix}-symbolic.svg
+            done;
     '';
 
   mkTelegramInstance = { name, displayName, workdir, iconsDir }:
@@ -28,24 +28,24 @@ let
       desktopItem = pkgs.makeDesktopItem {
         name = name;
         desktopName = displayName;
-	comment = "New era of messaging";
-	tryExec = "Telegram";
+        comment = "New era of messaging";
+        tryExec = "Telegram";
         exec = "Telegram -workdir ${workdir} -- %U";
         icon = if name == "telegram-personal" then "org.telegram.desktop" else name;
-	terminal = false;
+        terminal = false;
         startupWMClass = name;
         #startupWMClass = "TelegramDesktop";
-	type = "Application";
+        type = "Application";
         categories = [ "Chat" "Network" "InstantMessaging" "Qt" ];
-        mimeTypes = [ "x-scheme-handler/tg" "x-scheme-handler/tonsite"];
-	keywords = ["tg" "chat" "im" "messaging" "messenger" "sms" "tdesktop"];
-	actions.quit = {
-	  name = "Quit Telegram";
-	  exec = "Telegram -quit";
-	  icon = "application-exit";
-	};
-	dbusActivatable = true;
-	extraConfig = {
+        mimeTypes = [ "x-scheme-handler/tg" "x-scheme-handler/tonsite" ];
+        keywords = [ "tg" "chat" "im" "messaging" "messenger" "sms" "tdesktop" ];
+        actions.quit = {
+          name = "Quit Telegram";
+          exec = "Telegram -quit";
+          icon = "application-exit";
+        };
+        dbusActivatable = true;
+        extraConfig = {
           "DBusActivatable" = "false";
           "SingleMainWindow" = "true";
           "X-GNOME-UsesNotifications" = "true";
@@ -64,16 +64,17 @@ let
 
 in
 {
-  users.users.<username>.packages = with pkgs; [               #put here your username instead of <username>
+  users.users.<username>.packages = with pkgs; [
+    #put here your username instead of <username>
     telegram-desktop
 
     (mkTelegramInstance {
       name = "telegram-work";
       displayName = "Telegram (Work)";
-      workdir = lib.strings.join "/" ["${homePath}" ".local/share/TelegramDesktopWorking"];
+      workdir = lib.strings.join "/" [ "${homePath}" ".local/share/TelegramDesktopWorking" ];
       iconsDir = builtins.path {
-        path = lib.strings.join "/" ["${homePath}" "Pictures/icons/hicolor"];
-	name = "telegram-icons";
+        path = lib.strings.join "/" [ "${homePath}" "Pictures/icons/hicolor" ];
+        name = "telegram-icons";
       };
       #iconsDir = lib.strings.join "/" ["${homePath}" "Pictures/icons/hicolor"];
     })
@@ -84,10 +85,10 @@ in
     (mkTelegramInstance {
       name = "telegram-personal";
       displayName = "Telegram (Personal)";
-      workdir = lib.strings.join "/" ["${homePath}" ".local/share/TelegramDesktop"];
+      workdir = lib.strings.join "/" [ "${homePath}" ".local/share/TelegramDesktop" ];
       iconsDir = builtins.path {
-        path = lib.strings.join "/" ["${homePath}" "Pictures/icons/hicolor"];
-	name = "telegram-icons";
+        path = lib.strings.join "/" [ "${homePath}" "Pictures/icons/hicolor" ];
+        name = "telegram-icons";
       };
     })
   ];
